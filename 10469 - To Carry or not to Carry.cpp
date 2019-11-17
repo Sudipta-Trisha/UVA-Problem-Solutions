4d179b09a43dc7e9254e6029b25733b31e1cc5c6
Sudipta Banik Trisha <43735253+Sudipta-Trisha@users.noreply.github.com>
2019-11-17 22:51:20 +0600

    adhoc

---
+++ 10469 - To Carry or not to Carry.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    int a,b;
+    while(scanf("%d %d",&a,&b)!=EOF){
+        printf("%d\n",a^b);
+    }
+    return 0;
+}

4782531b22964745d44f24eb358281da8666e846
Sudipta Banik Trisha <43735253+Sudipta-Trisha@users.noreply.github.com>
2020-04-27 13:32:37 +0600

    Number Theory

---
+++ 10299 - Relatives.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+#define sz 1000000009
+
+ll phi_func(ll n)
+{
+    ll node = n;
+    for(ll i=2; i*i<=n; i++){
+        if(n%i==0){
+            while(n%i==0){
+                n/=i;
+            }
+            node = node - (node/i);
+        }
+    }
+    if(n>1){
+        node = node - (node/n);
+    }
+    return node;
+}
+
+int main()
+{
+    ll n;
+    while(scanf("%lld",&n) && n>0){
+        if(n==1) printf("0\n");
+        else printf("%lld\n",phi_func(n));
+    }
+    return 0;
+}
+
